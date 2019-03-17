7163fca639ade33202d613cbe8732cd27f3f23bd
Priya Kavathekar <priya.kavathekar@in.ibm.com>
2019-03-17 14:57:57 +0530

    updated the cart service

---
+++ src/main/java/net/jmesnil/numbers/bean/CartData.java
@@ -0,0 +1,27 @@
+package net.jmesnil.numbers.bean;
+
+import java.util.List;
+
+public class CartData {
+	
+	String id =null;
+	
+	List<Product> productList;
+
+	public List<Product> getProductList() {
+		return productList;
+	}
+
+	public void setProductList(List<Product> productList) {
+		this.productList = productList;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+}
